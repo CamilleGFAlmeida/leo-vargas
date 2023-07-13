246b3245f18e3477a5eb94d16342f36db38ff4c8
CamilleGFAlmeida <camilleguillenf.almeida@gmail.com>
2023-07-13 00:38:44 -0300

    updt

--- guardioes-da-galaxia/script/index.js
+++ guardioes-da-galaxia/script/index.js
@@ -6,3 +6,29 @@ const members = [
   { name: 'Rocket Raccoon', id: 'rocket' },
   { name: 'I am Groot', id: 'groot' },
 ]
+
+let activeMember = 0 
+const images = document.getElementById("images")
+
+function changeStatusButtons() {
+  const prev = document.getElementById('button__prev')
+  const isFirst = activeMember === 0
+  prev.disabled = isFirst
+
+  const next = document.getElementById("button__next")
+  const isLast = activeMember === members.length - 1
+  next.disabled = isLast
+
+}
+
+function navigationMember(direction) {
+  activeMember = activeMember + direction
+  
+  const member = members[activeMember]
+
+  console.log("activeMember", member); 
+
+  images.style.transform = `translateY(${-100 * activeMember}vh)`
+
+  changeStatusButtons()
+}
